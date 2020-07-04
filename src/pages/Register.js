98481d84d5330b7468e202e5b87362269a028125
inna_soldier <inna.soldier@gmail.com>
2020-07-04 16:56:14 -0500

    added login and register page + useform

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,29 @@
+/* eslint jsx-a11y/anchor-is-valid: 0 */
 import React from "react";
+import RegisterForm from '../components/auth/RegisterForm'
 
-const Register = () => <h1>I am Register page</h1>
+const Register = () => {
+    return (
+        <div className="auth-page">
+            <div className="container has-text-centered">
+                <div className="column is-4 is-offset-4">
+                    <h3 className="title has-text-grey">Register</h3>
+                    <p className="subtitle has-text-grey">Please Register to proceed.</p>
+                    <div className="box">
+                        <figure className="avatar">
+                            <img src="https://placehold.it/128x128"/>
+                        </figure>
+                        <RegisterForm />
+                    </div>
+                    <p className="has-text-grey">
+                        <a>Sign In With Google</a>&nbsp;
+                        <a href="/">Sign Up</a> &nbsp;·&nbsp;
+                        <a href="../">Need Help?</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default Register
